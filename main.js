900362687a695232836be2c912ca6f0e6b36abaf
Initdd <a50468@alunos.isel.pt>
2023-12-21 18:12:22 +0000

    save js code to a separate file

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-function test() {
-    let p = document.getElementById("i");
-    let clas = ["t1", "t2"];
-    let index = 0;
+// canvas
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d");
 
-    setInterval(() => {
-        p.className = clas[index];
-        index = (index + 1) % clas.length;
-    }, 1000);
+// coordinates
+let radius = 40
+let x = canvas.width/2
+let y = canvas.height-radius
+let velocity = -5 // how much to go each time
+let enable = true
+
+function drawCircle(xCoor, yCoor) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.beginPath();
+    ctx.arc(xCoor, yCoor, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function temp() {
+    drawCircle(x, y)
+    y = y + velocity
+}
+
+function goUp() {
+    if (enable) {
+        setInterval(() => {temp()}, 50)
+        enable = false
+    }           
 }
